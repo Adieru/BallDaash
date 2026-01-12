0a30f5988fe1398a6a07a67782c6de5dd8d201f2
Adieru <sushilaimadol@gmail.com>
2026-01-12 19:34:42 +0545

    Added minor changes still thinking abt how to implement infinite map

--- Assets/Script/NextMap.cs
+++ Assets/Script/NextMap.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class NextMap : MonoBehaviour
 {
-    /*
-     private void OnTriggerEnter(Collider other)
+
+    public Transform MapHolder;
+    Transform coinHolder;
+
+    private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("MapPass"))
-        { 
-            Debug.Log("Next Map Triggered");
-            // Here you can add code to load the next map or level
+        if (other.gameObject.CompareTag("MapPass"))
+        {
+
+            if (other.transform.GetSiblingIndex() == 1)
+            {
+                MapHolder.GetChild(0).transform.position = MapHolder.GetChild(2).transform.position
+                                                                + new Vector3(0, 0, 150);
+
+                /*
+                coinHolder = MapHolder.GetChild(0).GetChild(0);
+
+                for (int i = 0; i < coinHolder.childCount; i++)
+                {
+                    coinHolder.GetChild(i).gameObject.SetActive(true);
+                }
+                */
+
+                MapHolder.GetChild(0).SetSiblingIndex(2);
+
+
+
+            }
         }
     }
-    */
 
 }
