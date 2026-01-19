98245df0046dda4982152da3a137b6c77b55a4cc
Adieru <sushilaimadol@gmail.com>
2026-01-19 17:51:47 +0545

    Adding powerups

--- Assets/Script/PowerUps.cs
+++ Assets/Script/PowerUps.cs
@@ -1,21 +1,56 @@
 using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PowerUps : MonoBehaviour
+
 {
 
-    //Obstacle
-    GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle"); 
+    GameObject[] obstacles;
 
     private void OnTriggerEnter(Collider other)
+
     {
-        if(other.gameObject.CompareTag("Invisible"))
+
+        if (other.gameObject.CompareTag("Invisible"))
+
         {
-            for(int i = 0; i < obstacles.Length; i++)
-            {
-                obstacles[i].GetComponent<BoxCollider>().isTrigger = true;
-            }
+
+            StartCoroutine(ChangeInvisibleStatus());
+
         }
+
     }
+
+
+    IEnumerator ChangeInvisibleStatus()
+
+    {
+
+        obstacles = GameObject.FindGameObjectsWithTag("Obstacles");
+        BecomeInvis(true, 10);
+
+
+        yield return new WaitForSeconds(5);
+
+        BecomeInvis(false, 5);
+
+
+    }
+
+
+    void BecomeInvis(bool value, int playerSpeed)
+    {
+
+        for(int i = 0; i < obstacles.Length; i++)
+        {
+            obstacles[i].GetComponent<BoxCollider>().isTrigger = value;
+        }
+
+        this.GetComponent<PlayerMovement>().velocity = playerSpeed;
+    }
+
 }
+
